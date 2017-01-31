dfc65784046c18e72b381dc008f24c1108928d2d
LauraBerry <lauraberry963@hotmail.ca>
2017-01-31 10:09:20 -0700

    updated assignment code
    it should theoretically play a different buzzer sound for each force
    sensing resistor.

--- A2.py
+++ A2.py
@@ -3,34 +3,29 @@
 #http://www.hertaville.com/interfacing-an-spi-adc-mcp3008-chip-to-the-raspberry-pi-using-c.html
 #https://www.raspberrypi.org/forums/viewtopic.php?f=63&t=40515
 
+#http://raspberrypi.stackexchange.com/questions/14505/using-fsr-with-raspberry-pi
+#https://www.sunfounder.com/learn/Super_Kit_V2_for_RaspberryPi/lesson-6-buzzer-super-kit-for-raspberrypi.html
 
 import RPi.GPIO as GPIO;
 import time;
-import pyaudio; 
-import wave;
+import os;
+
 GPIO.VERSION;
 GPIO.setmode(GPIO.BCM);
 GPIO.setwarnings(False);
-drums = wave.open("ambi-115bpm-1a.wav");
-p=pyaudio.PyAudio();
-stream = p.open(format = p.get_format_from_width(drums.getsampwidth()), channels= drums.getnchannels(), rate= drums.getframerate(), output = True);
-data = drums.readframes(chunk);
-#pins for buttons
-#GPIO.setup(10,GPIO.IN);
-#GPIO.setup(11,GPIO.IN);
-#pins for knob
-GPIO.setup(12,GPIO.IN);
-GPIO.setup(13,GPIO.OUT);
-#pin for speaker(?)
-#GPIO.setup(17,GPIO.OUT);
-buttonAStatus =0;
-buttonBStatus =0;
-#buttonAPin=10;
-#buttonBPin=11;
-knobPinOne = 12;
-knobPinTwo = 13;
-knobValue = 0;
+Resistor1Pin=27;
+Resistor2Pin=28;
+BuzzerPin=11;
+GPIO.setup (Resistor1Pin, GPIO.IN);
+GPIO.setup(Resistor2Pin, GPIO.IN);
+GPIO.setup(BuzzerPin, GPIO.OUT);
 while True:
-	readADC();
-	stream.write(data);
-	data = drums.readframes(chunk);
+	reading =0;
+	if (GPIO.input(Resistor1Pin) == GPIO.LOW):
+		GPIO.output(BuzzerPin, GPIO.LOW)
+		time.sleep(0.1);
+		GPIO.output(BuzzerPin, False);
+	if (GPIO.input(Resistor2Pin) == GPIO.LOW):
+		GPIO.output(BuzzerPin, GPIO.HIGH);
+		time.sleep(0.1);
+		GPIO.output(BuzzerPin, False);
